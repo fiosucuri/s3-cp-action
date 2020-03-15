fffa6d12c07a25e15897e2e4f2ee6d9fd3525fc1
Fioravante Souza <fsouza@godaddy.com>
2020-03-15 11:30:31 -0300

    using aws configure instead of variable files

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,8 @@
 
 set -e
 
-mkdir -p ~/.aws
-touch ~/.aws/credentials
+aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+aws configure set default.region us-west-2
 
-echo "[default]
-aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
-
-aws s3 cp ${FILE} s3://${AWS_S3_BUCKET} \
-            --region ${AWS_REGION} $*
-
-rm -rf ~/.aws
+aws s3 cp ${FILE} s3://${AWS_S3_BUCKET}
